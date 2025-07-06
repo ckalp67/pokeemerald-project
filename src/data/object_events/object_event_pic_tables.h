590ba09153c71bee264bac5075ed2f9fcc7d2ea7
ckalp67 <ckalp67@gmail.com>
2025-07-06 19:48:20 -0400

    fixed pic table for female rocket grunt

--- src/data/object_events/object_event_pic_tables.h
+++ src/data/object_events/object_event_pic_tables.h
@@ -38,15 +38,15 @@ static const struct SpriteFrameImage sPicTable_Rocket_M[] = {
 };
 
 static const struct SpriteFrameImage sPicTable_Rocket_F[] = {
-    overworld_frame(gObjectEventPic_RocketGruntM, 2, 4, 0),
-    overworld_frame(gObjectEventPic_RocketGruntM, 2, 4, 1),
-    overworld_frame(gObjectEventPic_RocketGruntM, 2, 4, 2),
-    overworld_frame(gObjectEventPic_RocketGruntM, 2, 4, 3),
-    overworld_frame(gObjectEventPic_RocketGruntM, 2, 4, 4),
-    overworld_frame(gObjectEventPic_RocketGruntM, 2, 4, 5),
-    overworld_frame(gObjectEventPic_RocketGruntM, 2, 4, 6),
-    overworld_frame(gObjectEventPic_RocketGruntM, 2, 4, 7),
-    overworld_frame(gObjectEventPic_RocketGruntM, 2, 4, 8),
+    overworld_frame(gObjectEventPic_RocketGruntF, 2, 4, 0),
+    overworld_frame(gObjectEventPic_RocketGruntF, 2, 4, 1),
+    overworld_frame(gObjectEventPic_RocketGruntF, 2, 4, 2),
+    overworld_frame(gObjectEventPic_RocketGruntF, 2, 4, 3),
+    overworld_frame(gObjectEventPic_RocketGruntF, 2, 4, 4),
+    overworld_frame(gObjectEventPic_RocketGruntF, 2, 4, 5),
+    overworld_frame(gObjectEventPic_RocketGruntF, 2, 4, 6),
+    overworld_frame(gObjectEventPic_RocketGruntF, 2, 4, 7),
+    overworld_frame(gObjectEventPic_RocketGruntF, 2, 4, 8),
 };
 
 static const struct SpriteFrameImage sPicTable_BrendanUnderwater[] = {
