88def80e4742d4390626fc33fb562ea54e555dbb
ckalp67 <ckalp67@gmail.com>
2025-07-06 22:26:27 -0400

    alt dialogue and rocket grunts in devon

--- include/constants/opponents.h
+++ include/constants/opponents.h
@@ -332,8 +332,8 @@
 #define TRAINER_JAYLEN                      326
 #define TRAINER_DILLON                      327
 #define TRAINER_CALVIN_2                    328
-#define TRAINER_CALVIN_3                    329
-#define TRAINER_CALVIN_4                    330
+#define TRAINER_ROCKET_GRUNT_DEVON_3        329
+#define TRAINER_ROCKET_GRUNT_DEVON_2        330
 #define TRAINER_ROCKET_GRUNT_DEVON_1        331
 #define TRAINER_EDDIE                       332
 #define TRAINER_ALLEN                       333
